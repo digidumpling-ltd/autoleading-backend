d1f168874b81c3b278b852f01ec9c440d619aaf8
irfanCoda <irfan@codaai.tech>
2026-05-31 15:40:34 +0800

    feat(rewards): enhance rewards index layout and improve styling

--- packages/Webkul/Rewards/src/Resources/views/shop/customers/rewards/index.blade.php
+++ packages/Webkul/Rewards/src/Resources/views/shop/customers/rewards/index.blade.php
@@ -5,14 +5,33 @@
         @lang('rewards::app.shop.customer.account.rewards.index.your-reward-points')
     </x-slot>
 
-    <div class="flex items-center justify-between">
-        <div class="">
-            <h2 class="text-lg font-medium">
-                @lang('rewards::app.shop.customer.account.rewards.index.your-reward-points') {{ $totalRewardPoints }}
-            </h2>
-        </div>
+    <div class="max-md:hidden">
+        <x-shop::layouts.account.navigation />
     </div>
 
-    <x-shop::datagrid :src="route('customer.rewards.index')"></x-shop::datagrid>
+    <div class="mx-4 flex-auto max-md:mx-6 max-sm:mx-4">
+        <div class="mb-8 flex items-center max-sm:mb-5">
+            <!-- Back Button -->
+            <a
+                class="grid md:hidden"
+                href="{{ route('shop.customers.account.index') }}"
+            >
+                <span class="icon-arrow-left rtl:icon-arrow-right text-2xl"></span>
+            </a>
+
+            <h2 class="text-2xl font-medium ltr:ml-2.5 rtl:mr-2.5 max-sm:text-base md:ltr:ml-0 md:rtl:mr-0">
+                @lang('rewards::app.shop.customer.account.rewards.index.your-reward-points')
+            </h2>
+        </div>
+
+        <div class="mb-6 flex items-center justify-between">
+            <p class="text-lg font-medium">
+                @lang('rewards::app.shop.customer.account.rewards.index.your-reward-points')
+                <span class="font-semibold">{{ $totalRewardPoints }}</span>
+            </p>
+        </div>
+
+        <x-shop::datagrid :src="route('customer.rewards.index')"></x-shop::datagrid>
+    </div>
 
 </x-shop::layouts.account>
